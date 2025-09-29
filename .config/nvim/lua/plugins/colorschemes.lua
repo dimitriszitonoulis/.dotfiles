fe668ee93a41a6b826bf435083f64d84d2a84b6a
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-09-29 17:38:56 +0300

    changed plugin to load catpuccin as default theme

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,55 +1,90 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim", -- or another colorscheme plugin
-  --   lazy = false, -- load immediately
-  --   priority = 1000, -- load before other plugins
-  --   config = function()
-  --     vim.cmd([[colorscheme tokyonight]])
-  --   end,
-  -- }
+  -- tokyonight
   -- {
   --   "folke/tokyonight.nvim",
   --   lazy = false, -- load immediately
-  --
-  --   config = function()
-  --     require("tokyonight").setup({
-  --       -- style = "day",
-  --       -- style = "moon",
-  --       -- style = "storm",
-  --       style = "night",
-  --       transparent = false, -- make backround solid
-  --       styles = {
-  --         comments = { italic = false },
-  --         keywords = { italic = false },
-  --       },
-  --     })
-  --     vim.cmd([[colorscheme tokyonight]])
-  --
-  --     --For tokyonight-day
-  --     -- Set the cursor color to black (in GUI or terminal that supports it)
-  --     vim.api.nvim_set_hl(0, "Cursor", { fg = "white", bg = "black" })
+  --   opts = {
+  --     -- style = "day",
+  --     -- style = "moon",
+  --     -- style = "storm",
+  --     style = "night",
+  --     transparent = false, -- make backround solid
+  --     styles = {
+  --       comments = { italic = false },
+  --       keywords = { italic = false },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require("tokyonight").load(opts)
+  --     vim.cmd.colorscheme("tokyonight")
   --   end,
   -- },
 
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- load immediately
-
+    "catppuccin/nvim",
+    lazy = false,
+    -- priority = 1000, -- only needed if laze = true
+    name = "catppuccin",
     opts = {
-      -- style = "day",
-      -- style = "moon",
-      -- style = "storm",
-      style = "night",
-      transparent = false, -- make backround solid
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
+      lsp_styles = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        snacks = true,
+        telescope = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
+          end
+        end,
       },
     },
 
-    config = function(_, opts)
-      require("tokyonight").load(opts)
-      vim.api.nvim_set_hl(0, "Cursor", { fg = "white", bg = "black" })
-    end,
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
   },
+  -- Since I had custom options (like the behaviour of bufferline),
+  -- I chose to use the config option
+  -- The following can be used, but I must set lazy = true
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   }
+  -- }
 }
