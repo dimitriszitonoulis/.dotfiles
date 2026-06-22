97d9d10d6de2cde727944668336d1de7c5087938
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-06-22 23:39:20 +0300

    added QT environmental variables and updated documentation link for env variables

--- wayland/.config/hypr/hyprland.lua
+++ wayland/.config/hypr/hyprland.lua
@@ -47,9 +47,20 @@ require("wallpaper")
 --## ENVIRONMENT VARIABLES ###
 --############################
 
--- See https://wiki.hypr.land/Configuring/Environment-variables/
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+
 
 
 --------------------
